aedef73fe07acea508b5edf615e8820a19f82897
Onokaev <evansonoka05.j@gmail.com>
2023-11-04 13:40:17 +0300

    Open uri from web

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import { getToken } from './auth';
 import * as clipboard from 'clipboardy';
+import TelemetryReporter from '@vscode/extension-telemetry';
 
 export function activate(context: vscode.ExtensionContext) {
 	
@@ -13,6 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
 			vscode.window.showInformationMessage('Token copied to clipboard');
 		}
 	});
+	const reporter = new TelemetryReporter(context.extension.packageJSON.telemetryInstrumentationKey);
 	context.subscriptions.push(
 		vscode.window.registerUriHandler({
 			handleUri: async (uri: vscode.Uri) => {
@@ -20,7 +22,7 @@ export function activate(context: vscode.ExtensionContext) {
 					console.log('Hit default URI');
 					return;
 				}
-				if(uri.path.toLocaleLowerCase() === '/gettoken'){
+				if(uri.path.toLocaleLowerCase() === '/getToken'){
 					console.log('Hit getToken URI');
 					const authResult = await getToken();
 					if(authResult){
@@ -30,11 +32,41 @@ export function activate(context: vscode.ExtensionContext) {
 					}
 				}
 			}
+		}),
+		reporter,
+		registerCommandWithTelemetry(reporter, 'graphapponlytoken.generateToken', async () => {
+			const authResult = await getToken();
+			if(authResult){
+				clipboard.default.writeSync(authResult.accessToken);
+				vscode.window.showInformationMessage('Token copied to clipboard');
+			}
 		})
-	)
-
+	);
 	context.subscriptions.push(disposable);
 }
+
+function getQueryParameters(uri: vscode.Uri): Record<string, string> {
+	const query = uri.query;
+	if (!query) {
+	  return {};
+	}
+	const queryParameters = (query.startsWith('?') ? query.substring(1) : query).split("&");
+	const parameters = {} as Record<string, string>;
+	queryParameters.forEach((element) => {
+	  const keyValue = element.split("=");
+	  parameters[keyValue[0].toLowerCase()] = decodeURIComponent(keyValue[1]);
+	});
+	return parameters;
+  }
+
+  function registerCommandWithTelemetry(reporter: TelemetryReporter, command: string, callback: (...args: any[]) => any, thisArg?: any): vscode.Disposable {
+	return vscode.commands.registerCommand(command, (...args: any[]) => {
+	  const splatCommand = command.split('/');
+	  const eventName = splatCommand[splatCommand.length - 1];
+	  reporter.sendTelemetryEvent(eventName);
+	  return callback.apply(thisArg, args);
+	}, thisArg);
+  }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
